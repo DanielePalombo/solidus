cb790f49b197017878e78026edbcadae8792c595
Alberto Vena <kennyadsl@gmail.com>
2020-10-29 15:51:42 +0100

    Remove deprecated methods for payment sources
    They have been replaced by the new wallet methods.

--- core/app/models/concerns/spree/user_payment_source.rb
+++ core/app/models/concerns/spree/user_payment_source.rb
@@ -4,23 +4,8 @@ module Spree
   module UserPaymentSource
     extend ActiveSupport::Concern
 
-    def default_credit_card
-      Spree::Deprecation.warn(
-        "user.default_credit_card is deprecated. Please use user.wallet.default_wallet_payment_source instead.",
-        caller
-      )
-      default = wallet.default_wallet_payment_source
-      if default && default.payment_source.is_a?(Spree::CreditCard)
-        default.payment_source
-      end
-    end
-
-    def payment_sources
-      Spree::Deprecation.warn(
-        "user.payment_sources is deprecated. Please use user.wallet.wallet_payment_sources instead.",
-        caller
-      )
-      credit_cards.with_payment_profile
+    included do
+      Spree::Deprecation.warn('Spree::UserPaymentSource is deprecated, please stop including it.', caller)
     end
   end
 end
